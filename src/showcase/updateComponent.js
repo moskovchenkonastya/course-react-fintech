2b0eccea898d52a1bb063c6c7c7594770c69c172
Nastya <nastya.popper@gmail.com>
2017-11-25 16:21:37 +0300

    fix err

--- src/showcase/updateComponent.js
+++ src/showcase/updateComponent.js
@@ -5,6 +5,6 @@ export default class updateComponent extends React.Component {
     }
   
     render() {
-      return (<div> {this.props.children} </div>);
+      return (<div>{this.props.children}</div>);
     }
 } 
