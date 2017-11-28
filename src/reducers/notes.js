c03711d2ae675ff7a051bee465480e35c165b41d
Nastya <nastya.popper@gmail.com>
2017-11-29 01:07:03 +0300

    done

--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -4,6 +4,8 @@ export const REORDER_NOTE = 'REORDER_NOTE'
 export const UPDATE_NOTE = 'UPDATE_NOTE'
 export const DELETE_ALL_NOTES = 'DELETE_ALL_NOTES'
 
+const i = 0
+
 export default (state = [], action) => {
     switch(action.type) {
         
@@ -11,25 +13,25 @@ export default (state = [], action) => {
           return [...state, action.note];
         
       case REMOVE_NOTE:
-        const i = state.findIndex((cur) => (cur.id === action.id));
+        i = state.findIndex((cur) => (cur.id === action.id));
         state.splice(i, 1);
         return state;
 
       case REORDER_NOTE:
         return state.map(cur => {
           if (cur.id === action.fromId) {
-            const i = state.findIndex(cur => cur.id === action.toId);
+            i = state.findIndex(cur => cur.id === action.toId);
             return state[i];
           }
           if (cur.id === action.toId) {
-            const i = state.findIndex(cur => cur.id === action.fromId);
+            i = state.findIndex(cur => cur.id === action.fromId);
             return state[i];
           }
           return cur;
         });
-        
+
       case UPDATE_NOTE:
-        const i = state.findIndex(cur => cur.id === action.note.id);
+        i = state.findIndex(cur => cur.id === action.note.id);
         if (i !== -1) {
           return [...state.slice(0, i), action.note, ...state.slice(i + 1)];
         }
